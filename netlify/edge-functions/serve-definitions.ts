5048faa3a0c0b69b071025317f6bf9144045b100
Sergio Moya <1083296+smoya@users.noreply.github.com>
2022-04-19 23:33:13 +0200

    test edge functions v2

--- netlify/edge-functions/serve-definitions.ts
+++ netlify/edge-functions/serve-definitions.ts
@@ -2,4 +2,10 @@ import type { Context } from "netlify:edge";
 
 export default async (request: Request, context: Context) => {
   context.log("Hello from the logging service");
+
+  // Setting proper Content-Type header for JSON Schema files 
+  const response = await context.next();
+  response.headers.set("Content-Type", "application/schema+json");
+  
+  return response;
 };
